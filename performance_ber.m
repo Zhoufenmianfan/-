69d87e072bc8e5f329e30e1016f2772ea52e45b7
Porio <huzhouyong@gmail.com>
2022-03-07 15:14:34 +0800

    最初版本1.0

---
+++ performance_ber.m
@@ -0,0 +1,20 @@
+clear all;
+
+rep = 1000;%repetition times
+snr = 0:1:10;
+
+ber=zeros(size(snr,2),rep);
+
+for j = 1:rep
+    waitbar(j/rep); 
+
+  for i = 1:length(snr)
+      [ber(i,j)] = General_MIMO(2,3,snr(i));
+
+   end
+end
+
+semilogy(snr,mean(ber,2),'b-*');
+xlabel('SNR');
+ylabel('BER');
+grid;
